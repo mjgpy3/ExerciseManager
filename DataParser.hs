42c87a0c8906d25ac0b6bacc2710c1851e9e3b58
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-07 23:43:26 -0400

    Added some type declarations

--- DataParser.hs
+++ DataParser.hs
@@ -3,9 +3,11 @@ module DataParser where
 import ExerciseManager
 import Data.List.Split
 
+parseDate :: String -> Date
 parseDate d = Date (read $ head dl) (read $ dl !! 1) (read $ last dl)
     where dl = splitOn "/" d
 
+parseTime :: String -> Time
 parseTime t = Time (read $ head tl) (read $ tl !! 1) (read $ last tl) (last tsplit)
     where tsplit = splitOn " " t
           tl = splitOn ":" (head tsplit)
