182d7ebe30e21ebf517bc955ab167e7a2e8f79dc
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-09 09:39:11 -0400

    Did some revamping

---
+++ WorkoutLog.hs
@@ -0,0 +1,9 @@
+module WorkoutLog where
+
+import ExerciseData
+
+workouts = 
+ [ BasicWorkout [RepWorkout 10 7, RepWorkout 10 7] (Datetime (Date 5 6 2013) (Time 6 0 0 "PM")),
+   BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 7 2013) (Time 6 10 0 "PM")),
+   BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 8 2013) (Time 6 10 0 "PM"))
+ ]
