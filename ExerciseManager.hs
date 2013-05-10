070acff19c49b3ccfe171237a0e11148cf442395
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-10 03:09:36 -0400

    Got rid of unneeded code in ExerciseManager.hs

--- ExerciseManager.hs
+++ ExerciseManager.hs
@@ -4,16 +4,5 @@ import WorkoutFileReader
 import ExerciseData
 import DataParser
 
---workoutsFromCSV csv = [ BasicWorkout (parseDate (head header)) 
---                                (parseTime (last header))
-  --                              [RepWorkout 1 2]
-    --                            | l <- readCSV csv,
-      --                            let (header, exercise) = splitAt 2 (tail l) ]
-
---main = tail =<< readCSV "workout_file.csv"
-
-tail' [] = []
-tail' (x:xs) = xs
-
 main = do text <- readCSV "./workout_file.csv"
           tail' text
