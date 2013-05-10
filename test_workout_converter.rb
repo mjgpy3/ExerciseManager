03d41e4f5e4acc83ed8a5e8f534f388ead945e8f
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-09 20:51:00 -0400

    Started coding the workout file converter in ruby using TDD

--- test_workout_converter.rb
+++ test_workout_converter.rb
@@ -2,3 +2,24 @@
 
 require 'test/unit'
 require './workout_converter'
+
+class TestWorkoutConverter < Test::Unit::TestCase
+
+  def test_reading_a_workout_file_works
+    wc = WorkoutConverter.new
+    wc.read_workout_file "./TestFiles/testfile1.csv"
+
+    answer = wc.text
+    expected = "this is a simple file\nwith a few lines\n"
+
+    assert_equal expected, answer
+  end
+
+  def test_reading_a_workout_file_correctly_gets_headers
+    wc = WorkoutConverter.new
+    wc.read_workout_file "./TestFiles/testfile2.csv"
+
+    assert_equal ["Date","Time","Push Up Sets","Reps / Set","Sit Up Sets","Reps / Set"], wc.headers
+  end
+
+end
--- workout_converter.rb
+++ workout_converter.rb
@@ -1,3 +1,24 @@
 #!/usr/bin/env ruby
 
+class WorkoutConverter
+  def read_workout_file file_name
+    file = File.new file_name, 'r'
+    @text = file.read
+    @headers = get_headers
+    file.close
+  end
 
+  def text
+    @text
+  end
+
+  def headers
+    @headers
+  end
+
+private
+
+  def get_headers
+    @text.split("\n")[0].split(",")
+  end
+end
