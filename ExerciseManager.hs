12792b48e894ff5341f3905400bd07d459ae8544
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-10 03:32:33 -0400

    Made a summer in the main file (ExerciseManager)

--- ExerciseManager.hs
+++ ExerciseManager.hs
@@ -1,7 +1,14 @@
 module ExerciseManager where
 
-import WorkoutFileReader
 import ExerciseData
-import DataParser
+import WorkoutLog
+import System.Environment
 
+handleArgs args
+ | length args /= 2 = error "Usage: runhaskell ExerciseManager.hs [task] [option]"
+ | otherwise = case (head args) of
+    "t" -> return (sum [totalReps ((repWorkouts i) !! (read (last args))) | i <- workouts])
 
+main = do
+    a <- getArgs
+    handleArgs a
