54f5d76cde9aab6ebe2a18aa0d8722f718027a64
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-11 20:00:27 -0400

    Modded README, updated workout files

--- README.md
+++ README.md
@@ -2,6 +2,3 @@ ExerciseManager
 ----
 
 A Haskell breakable toy that processes queries on a simple CSV file.
-
-TODO:
-Write a tool that converts csv logs to Haskell source files
--- WorkoutLog.hs
+++ WorkoutLog.hs
@@ -7,5 +7,7 @@ workouts =
     BasicWorkout [RepWorkout 10 7, RepWorkout 10 7] (Datetime (Date 5 6 2013) (Time 6 0 0 "PM")),
     BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 7 2013) (Time 6 10 0 "PM")),
     BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 8 2013) (Time 6 10 0 "PM")),
-    BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 9 2013) (Time 7 20 0 "PM"))
+    BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 9 2013) (Time 7 20 0 "PM")),
+    BasicWorkout [RepWorkout 10 2, RepWorkout 10 3] (Datetime (Date 5 10 2013) (Time 11 0 0 "PM")),
+    BasicWorkout [RepWorkout 10 6, RepWorkout 10 6] (Datetime (Date 5 11 2013) (Time 7 45 0 "PM"))
   ]
