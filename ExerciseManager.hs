0f5b61b2e3dfe3261b907ca0ca30434fb30059ae
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-15 18:42:13 -0400

    Started changing the sturcture of 'workouts' (now a tuple). Deleted redundant file

--- ExerciseManager.hs
+++ ExerciseManager.hs
@@ -7,7 +7,8 @@ import System.Environment
 handleArgs args
  | length args /= 2 = error "Usage: runhaskell ExerciseManager.hs [task] [option]"
  | otherwise = case (head args) of
-    "t" -> return (sum [totalReps ((repWorkouts i) !! (read (last args))) | i <- workouts])
+    "t" -> return (sum [totalReps ((repWorkouts i) !! (read (last args))) | 
+                        i <- workouts])
 
 main = do
     a <- getArgs
--- TestFiles/WorkoutLog.hs
+++
@@ -1,9 +0,0 @@
-module WorkoutLog where
-
-import ExerciseData
-
-workouts = 
-  [
-    BasicWorkout [RepWorkout 10 7, RepWorkout 10 7] (Datetime (Date 5 6 2013) (Time 6 0 0 "PM")),
-    BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 7 2013) (Time 6 10 0 "PM"))
-  ]
