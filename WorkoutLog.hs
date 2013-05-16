c2532fb76c8d7ec0a3ac116c6f20682d90fa04a9
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-16 00:47:24 -0400

    Added more workout data

--- WorkoutLog.hs
+++ WorkoutLog.hs
@@ -3,7 +3,7 @@ module WorkoutLog where
 import ExerciseData
 
 workouts = 
-  [
+  (["Push Up Sets", "Sit Up Sets"],[
     BasicWorkout [RepWorkout 10 7, RepWorkout 10 7] (Datetime (Date 5 6 2013) (Time 6 0 0 "PM")),
     BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 7 2013) (Time 6 10 0 "PM")),
     BasicWorkout [RepWorkout 10 5, RepWorkout 10 5] (Datetime (Date 5 8 2013) (Time 6 10 0 "PM")),
@@ -12,5 +12,6 @@ workouts =
     BasicWorkout [RepWorkout 10 6, RepWorkout 10 6] (Datetime (Date 5 11 2013) (Time 7 45 0 "PM")),
     BasicWorkout [RepWorkout 10 7, RepWorkout 10 7] (Datetime (Date 5 13 2013) (Time 4 45 0 "AM")),
     BasicWorkout [RepWorkout 10 17, RepWorkout 10 17] (Datetime (Date 5 15 2013) (Time 10 0 0 "AM")),
-    BasicWorkout [RepWorkout 10 9, RepWorkout 10 9] (Datetime (Date 5 15 2013) (Time 2 20 0 "PM"))
-  ]
+    BasicWorkout [RepWorkout 10 9, RepWorkout 10 9] (Datetime (Date 5 15 2013) (Time 2 20 0 "PM")),
+    BasicWorkout [RepWorkout 10 4, RepWorkout 10 4] (Datetime (Date 5 16 2013) (Time 12 20 0 "AM"))
+  ])
