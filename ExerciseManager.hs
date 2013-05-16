adc8fd2d90b8b69bd40b4e63fdecce30850f3c43
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-16 00:59:38 -0400

    Added an interesting little menu

--- ExerciseManager.hs
+++ ExerciseManager.hs
@@ -8,8 +8,14 @@ handleArgs args
  | length args /= 2 = error "Usage: runhaskell ExerciseManager.hs [task] [option]"
  | otherwise = case (head args) of
     "t" -> return (sum [totalReps ((repWorkouts i) !! (read (last args))) | 
-                        i <- workouts])
+                        i <- (snd workouts)])
+
+printMenu = putStrLn (getLineString [t | t <- (zip [0..] (fst workouts))])
+
+getLineString [] = ""
+getLineString (x:xs) = (show (fst x)) ++ " - " ++ (snd x) ++ "\n" ++ (getLineString xs)
 
 main = do
+    printMenu
     a <- getArgs
     handleArgs a
